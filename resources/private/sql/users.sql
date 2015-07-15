f22feb31acb91d4b0c52a778fda54837c58794e2
aleksanderesk <aleksanderesk@gmail.com>
2015-07-15 13:42:36 -0500

    made users email a unique field

--- resources/private/sql/users.sql
+++ resources/private/sql/users.sql
@@ -2,7 +2,12 @@
 -- Initializes the table for users
 CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-    email VARCHAR(32) NOT NULL, 
+    email VARCHAR(32) NOT NULL UNIQUE, 
     password BINARY(60) NOT NULL,
     PRIMARY KEY(id)
 )
+
+-- name: select-password
+-- Gets password for a given username
+SELECT password FROM users 
+    WHERE email = :username
